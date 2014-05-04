a243fd6ab4c1547178af05ebd601de3f5c20895f
Steven Finley <steve.a.finley@gmail.com>
2014-05-03 20:16:23 -0500

    merged with master

---
+++ app/models/bands.js
@@ -0,0 +1,20 @@
+'use strict';
+/*
+module.exports = Artist;
+var bands = global.nss.db.collection('bands');
+var Mongo = require('mongodb');
+var _ = require('lodash');
+var fs = require('fs');
+var path = require('path');
+function Band(band){
+  this.name = band.name;
+  this.bio = band.bio;
+  this.address = band.address;
+  this.members = band.members;
+  this.bandPhoto = band.photo;
+  this.creator = '';
+  this.sponsors = [];
+  this.agent = '';
+}
+*/
+
